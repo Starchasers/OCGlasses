980094373f11256b90ce487b7a33cec6fc1ace03
marcin212 <m-d14@o2.pl>
2014-10-15 22:25:33 +0200

    mod init

---
+++ src/main/java/com/bymarcin/ocglasses/OCGlasses.java
@@ -0,0 +1,20 @@
+package com.bymarcin.ocglasses;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = OCGlasses.MODID, version = OCGlasses.VERSION)
+public class OCGlasses
+{
+    public static final String MODID = "ocglasses";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
