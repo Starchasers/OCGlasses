dc770aa12fa75ff5606ed61de6072c36698893a2
Łukasz Magiera <magik6k@gmail.com>
2014-11-08 21:49:37 +0100

    Added basic client side surface

---
+++ src/main/java/com/bymarcin/ocglasses/surface/Widgets.java
@@ -0,0 +1,10 @@
+package com.bymarcin.ocglasses.surface;
+
+public enum Widgets {
+	TRIANGLE("Triangle", 6),
+	QUAD("Quad", 7);
+	
+	private Widgets(String name, int parameters) {
+		// TODO Auto-generated constructor stub
+	}
+}
