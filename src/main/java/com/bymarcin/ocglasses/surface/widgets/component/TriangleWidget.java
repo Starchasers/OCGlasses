f473b572dd3cd496659aae228512b852b81ae639
Łukasz Magiera <magik6k@gmail.com>
2014-11-19 00:15:15 +0100

    Added Triangle

--- src/main/java/com/bymarcin/ocglasses/surface/widgets/component/TriangleWidget.java
+++ src/main/java/com/bymarcin/ocglasses/surface/widgets/component/TriangleWidget.java
@@ -1,11 +1,10 @@
 package com.bymarcin.ocglasses.surface.widgets.component;
 
+import io.netty.buffer.ByteBuf;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.nbt.NBTTagCompound;
 
 import org.lwjgl.opengl.GL11;
-
-import io.netty.buffer.ByteBuf;
 
 import com.bymarcin.ocglasses.lua.LuaObjectBuilder;
 import com.bymarcin.ocglasses.surface.IRenderableWidget;
@@ -13,73 +12,74 @@ import com.bymarcin.ocglasses.surface.IWidget;
 import com.bymarcin.ocglasses.surface.Widgets;
 import com.bymarcin.ocglasses.surface.widgets.atribute.IAlpha;
 import com.bymarcin.ocglasses.surface.widgets.atribute.IColorizable;
-import com.bymarcin.ocglasses.surface.widgets.atribute.IPositionable;
-import com.bymarcin.ocglasses.surface.widgets.atribute.IResizable;
 import com.bymarcin.ocglasses.surface.widgets.luafunction.GetAlpha;
 import com.bymarcin.ocglasses.surface.widgets.luafunction.GetColor;
-import com.bymarcin.ocglasses.surface.widgets.luafunction.GetPosition;
 import com.bymarcin.ocglasses.surface.widgets.luafunction.GetSize;
 import com.bymarcin.ocglasses.surface.widgets.luafunction.SetAlpha;
 import com.bymarcin.ocglasses.surface.widgets.luafunction.SetColor;
-import com.bymarcin.ocglasses.surface.widgets.luafunction.SetPosition;
 import com.bymarcin.ocglasses.surface.widgets.luafunction.SetSize;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class SquareWidget implements IWidget,IPositionable,IResizable,IColorizable,IAlpha{
-
-	float x;
-	float y;
-	float width;
-	float height;
+public class TriangleWidget implements IWidget,IColorizable,IAlpha{
+	float x1, x2, x3;
+	float y1, y2, y3;
 	float alpha = 1;
 	float r;
 	float g;
 	float b;
 	
-	public SquareWidget() {
+	public TriangleWidget() {
 	}
 	
-	public SquareWidget(float x, float y, float r, float g, float b) {
-		this.x = x;
-		this.y = y;
+	public TriangleWidget(float x1, float x2, float x3, float y1, float y2, float y3, float r, float g, float b) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
 		this.r = r;
 		this.g = g;
 		this.b = b;
 	}
-	public SquareWidget(double x, double y, double r, double g, double b){
-		this((float)x,(float)y,(float)r,(float)g,(float)b);
+	public TriangleWidget(double x1, double x2, double x3, double y1, double y2, double y3, double r, double g, double b){
+		this((float) x1, (float) x2, (float) x3, (float) y1, (float) y2, (float) y3, (float) r, (float) g, (float) b);
 	}
 	
 	
 	@Override
 	public void write(ByteBuf buff) {
-		buff.writeFloat(x);
-		buff.writeFloat(y);
+		buff.writeFloat(x1);
+		buff.writeFloat(x2);
+		buff.writeFloat(x3);
+		buff.writeFloat(y1);
+		buff.writeFloat(y2);
+		buff.writeFloat(y3);
 		buff.writeFloat(r);
 		buff.writeFloat(g);
 		buff.writeFloat(b);
-		buff.writeFloat(height);
-		buff.writeFloat(width);
 		buff.writeFloat(alpha);
 	}
 
 	@Override
 	public void read(ByteBuf buff) {
-		x = buff.readFloat();
-		y = buff.readFloat();
+		x1 = buff.readFloat();
+		x2 = buff.readFloat();
+		x3 = buff.readFloat();
+		y1 = buff.readFloat();
+		y2 = buff.readFloat();
+		y3 = buff.readFloat();
 		r = buff.readFloat();
 		g = buff.readFloat();
 		b = buff.readFloat();
-		height = buff.readFloat();
-		width = buff.readFloat();
 		alpha = buff.readFloat();
 	}
 	
 	@Override
 	public Widgets getType() {
-		return Widgets.QUAD;
+		return Widgets.TRIANGLE;
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -97,12 +97,11 @@ public class SquareWidget implements IWidget,IPositionable,IResizable,IColorizab
 			GL11.glDisable(GL11.GL_ALPHA_TEST);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			Tessellator tessellator = Tessellator.instance;
-			tessellator.startDrawingQuads();
+			tessellator.startDrawing(GL11.GL_TRIANGLES);
 			tessellator.setColorRGBA_F(r, g, b, alpha);
-			tessellator.addVertex(x, y, 0);
-			tessellator.addVertex(x, y+width, 0);
-			tessellator.addVertex(x+height, y+width, 0);
-			tessellator.addVertex(x+height, y+0, 0);
+			tessellator.addVertex(x1, y1, 0);
+			tessellator.addVertex(x2, y2, 0);
+			tessellator.addVertex(x3, y3, 0);
 			tessellator.draw();
 			GL11.glDisable(GL11.GL_BLEND);
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
@@ -112,71 +111,37 @@ public class SquareWidget implements IWidget,IPositionable,IResizable,IColorizab
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
-		nbt.setFloat("x", x);
-		nbt.setFloat("y", y);
+		nbt.setFloat("x1", x1);
+		nbt.setFloat("x2", x2);
+		nbt.setFloat("x3", x3);
+		nbt.setFloat("y1", y1);
+		nbt.setFloat("y2", y2);
+		nbt.setFloat("y3", y3);
 		nbt.setFloat("r", r);
 		nbt.setFloat("g", g);
 		nbt.setFloat("b", b);	
-		nbt.setFloat("width", width);
-		nbt.setFloat("height", height);
 		nbt.setFloat("alpha", alpha);
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
-		x = nbt.getFloat("x");
-		y = nbt.getFloat("y");
+		x1 = nbt.getFloat("x1");
+		x2 = nbt.getFloat("x2");
+		x3 = nbt.getFloat("x3");
+		y1 = nbt.getFloat("y1");
+		y2 = nbt.getFloat("y2");
+		y3 = nbt.getFloat("y3");
 		r = nbt.getFloat("r");
 		g = nbt.getFloat("g");
 		b = nbt.getFloat("b");
-		width = nbt.getFloat("width");
-		height = nbt.getFloat("height");
 		alpha = nbt.getFloat("alpha");
 	}
 
 	@Override
 	public Object[] getLuaObject(LuaObjectBuilder builder) {
-		builder.addFunction("setPosition", new SetPosition());
-		builder.addFunction("getPosition", new GetPosition());
-		builder.addFunction("setSize", new SetSize());
-		builder.addFunction("getSize", new GetSize());
-		builder.addFunction("setAlpha", new SetAlpha());
-		builder.addFunction("getAlpha", new GetAlpha());
 		builder.addFunction("setColor", new SetColor());
 		builder.addFunction("getColor", new GetColor());
 		return builder.createLuaObject();
-	}
-
-	@Override
-	public double getPosX() {
-		return x;
-	}
-
-	@Override
-	public double getPosY() {
-		return y;
-	}
-
-	@Override
-	public void setPos(double x, double y) {
-		this.x = (float) x;
-		this.y = (float) y;
-	}
-
-	@Override
-	public void setSize(double width, double height) {
-		this.height = (float) height;
-		this.width = (float) width;
-	}
-
-	@Override
-	public double getWidth() {
-		return width;
-	}
-
-	@Override
-	public double getHeight() {
-		return height;
 	}
 
 	@Override
@@ -210,5 +175,4 @@ public class SquareWidget implements IWidget,IPositionable,IResizable,IColorizab
 	public void setAlpha(double alpha) {
 		this.alpha = (float) alpha;
 	}
-	
 }
