4c6e4fc3843b8174ee745202d55731d6fed79511
marcin212 <m-d14@o2.pl>
2014-11-20 17:19:17 +0100

    Add utils for color Converter

---
+++ src/main/java/com/bymarcin/openglasses/utils/OGUtils.java
@@ -0,0 +1,16 @@
+package com.bymarcin.openglasses.utils;
+
+public class OGUtils {
+	
+	public static int getIntFromColor(float red, float green, float blue, float alpha){
+	    int R = Math.round(255 * red);
+	    int G = Math.round(255 * green);
+	    int B = Math.round(255 * blue);
+	    int A = Math.round(255 * alpha);
+	    A = (A << 24) & 0xFF000000;
+	    R = (R << 16) & 0x00FF0000;
+	    G = (G << 8) & 0x0000FF00;
+	    B = B & 0x000000FF;
+	    return A | R | G | B;
+	}
+}
