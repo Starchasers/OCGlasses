e0fcd5c3d7ba47492b288b8f0ae6585b2bff370c
marcin212 <m-d14@o2.pl>
2015-02-06 05:15:30 +0100

    added setColor

--- src/main/java/com/bymarcin/openglasses/testRender/Model.java
+++ src/main/java/com/bymarcin/openglasses/testRender/Model.java
@@ -6,7 +6,8 @@ import java.util.PriorityQueue;
 public class Model {
 	ArrayList<Matrix> vertexBuffer = new ArrayList<Matrix>();
 	PriorityQueue<Matrix> transformation = new PriorityQueue<Matrix>();
-
+	Matrix color = new Matrix(1, 4);
+	
 	public Model() {
 		transformation.add(Matrix.generateIdentityMatrix(4));
 	}
@@ -15,13 +16,24 @@ public class Model {
 		transformation.add(t);
 	}
 
+	public void setColor(float r, float g, float b, float alpha){
+		color.set(0, 0, r);
+		color.set(0, 1, g);
+		color.set(0, 2, b);
+		color.set(0, 3, alpha);
+	}
+	
 	public void addVertex(float x, float y, float z) {
 		Matrix vertex = new Matrix(1, 4);
 		vertex.set(0, 0, x);
 		vertex.set(0, 1, y);
 		vertex.set(0, 2, z);
 		vertex.set(0, 3, 1);
-		vertexBuffer.add(Matrix.multiply(transformation.peek(), vertex));
+		addVertex(Matrix.multiply(transformation.peek(), vertex));
+	}
+	
+	private void addVertex(Matrix vertex){
+		//TODO zamiana z 4 na 3, dodanie koloru, wrzucenie do bufora.
 	}
 
 	public void translate(float x, float y, float z) {
