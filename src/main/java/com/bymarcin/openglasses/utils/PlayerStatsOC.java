42ba5e7d40e0eebe8636adc131ffe3d5733ee12f
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2019-07-04 20:13:11 +0200

    ....

--- src/main/java/com/bymarcin/openglasses/utils/PlayerStatsOC.java
+++ src/main/java/com/bymarcin/openglasses/utils/PlayerStatsOC.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 
 public class PlayerStatsOC extends ben_mkiv.commons0815.utils.PlayerStats {
     public boolean nightVisionActive = false; //this is used to keep track of nv state for the unsubscribe event where we cant edit the NBT of the glasses anymore
+    public boolean infraredActive = false;
 
     public Conditions conditions;
 
